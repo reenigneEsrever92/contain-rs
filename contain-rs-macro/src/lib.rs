ff2c5f899cbad8d369ff2931d32d2ba3392c76c0
Felix Marezki <fmarezki@gmail.com>
2022-05-03 19:02:28 +0200

    wip

--- contain-rs-macro/src/lib.rs
+++ contain-rs-macro/src/lib.rs
@@ -2,7 +2,7 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, ExprCall, ExprLit};
+use syn::{spanned::Spanned, Attribute, DeriveInput, ExprAssign, ExprCall, ExprLit, Result};
 
 #[proc_macro_derive(Container, attributes(container, env_var))]
 pub fn container(item: TokenStream) -> TokenStream {
@@ -17,13 +17,62 @@ pub fn container(item: TokenStream) -> TokenStream {
     // println!("ARGS AST: {:#?}", args_ast);
     println!("ITEM AST: {:#?}", item_ast);
 
+    let model = parse_derive_input(item_ast);
+
     // let result = generate_into_container(&item_ast, &args_ast);
 
-    // println!("OUTPUT: {}", result);
+    println!("OUTPUT: {:#?}", model);
 
     // result
 
     TokenStream::new()
+}
+
+#[derive(Debug)]
+struct Model {
+    image: String,
+    env_vars: Vec<(String, String)>,
+}
+
+fn parse_derive_input(ast: DeriveInput) -> Result<Model> {
+    let attr = get_container_attribute(&ast)?;
+
+    Ok(Model {
+        image: "test".to_string(),
+        env_vars: Vec::new(),
+    })
+}
+
+fn parse_attribute(attr: &Attribute) {
+    
+}
+
+fn get_container_attribute<'a>(input: &'a DeriveInput) -> Result<&'a Attribute> {
+    let attrs = input.attrs.iter().fold(None, |left, right| match left {
+        Some(thing) => match thing {
+            Ok(_) => Some(Err(syn::Error::new(
+                right.span(),
+                "Expected only one container annotation",
+            ))),
+            Err(mut e) => {
+                e.combine(syn::Error::new(
+                    right.span(),
+                    "Expected only one container annotation",
+                ));
+
+                Some(Err(e))
+            }
+        },
+        None => Some(Ok(right)),
+    });
+
+    match attrs {
+        Some(result) => result,
+        None => Err(syn::Error::new(
+            input.span(),
+            "Expected container annotation",
+        )),
+    }
 }
 
 fn generate_into_container(item: &syn::DeriveInput, container_builder: &ExprCall) -> TokenStream {
@@ -50,7 +99,7 @@ fn env(args: TokenStream, item: TokenStream) -> TokenStream {
 
     println!("ARGS AST: {:#?}", args_ast);
     println!("ITEM AST: {:#?}", item_ast);
-    
+
     let output = generate_env_impl(&args_ast, &item_ast);
 
     println!("OUTPUT: {}", output);
@@ -64,7 +113,7 @@ fn generate_env_impl(args: &ExprLit, item: &DeriveInput) -> TokenStream {
     quote! {
         #item
 
-        
+
     }
     .into()
 }
